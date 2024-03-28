a8089a0089973437a1d84d8c1617ffcfa6275246
murilo.bernardo <murilo.b@hotmail.com>
2024-03-27 21:47:01 -0300

    Adicionando instruções de acesso

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ const fncAbreContato = () => {
 
 const fncTutorialCelular = () => {
     console.log('Abrindo manual de acesso para celular...')
-    window.open('tutoriais/acessandoPeloCelular.pdf','_blank');
+    window.location.href = 'celular.html'; 
 }
 
 const fncTutorialComputador = () => {
     console.log('Abrindo manual de acesso para computador...')
+    window.location.href = 'computador.html'; 
+}
+
+const fncDeVoltaAoInicio = () => { 
+    window.location.href = 'index.html'; 
+}
+
+const baixarTutorialCelular = () => {
+    console.log('Baixando manual de acesso para celular...') 
+    window.open('tutoriais/acessandoPeloCelular.pdf','_blank');
+}
+
+const baixarTutorialComputador = () => {
+    console.log('Baixando manual de acesso para computador...') 
     window.open('tutoriais/acessandoPeloComputador.pdf','_blank');
 }
